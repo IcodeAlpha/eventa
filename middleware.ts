46c7dae2f093d636472efb393a8625ff7ccbabbe
Your Name <alphamunene5@gmail.com>
2024-05-20 16:04:28 +0300

    first commit

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+
+const eventsProtected = createRouteMatcher(["/events(.*)"])
+const webhookProtected = createRouteMatcher(["/api/webhook(.*)"])
+
+// export default clerkMiddleware({
+//     publicRoutes: [
+//         '/',
+//         '/events/id',
+//         '/api/webhook/clerk',
+//         '/api/webhook/stripe',
+//         '/api/uploadthing'
+//     ],
+//     ignoredRoutes: [
+//         '/api/webhook/clerk',
+//         '/api/webhook/stripe',
+//         '/api/uploadthing'
+//     ]
+// });
+
+export default clerkMiddleware((auth, request) => {
+    if (eventsProtected(request)) auth().protect()
+    if (webhookProtected(request)) auth().protect()
+  })
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
