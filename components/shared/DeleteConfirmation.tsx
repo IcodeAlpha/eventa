1ca8c225cf846c8ff46651208a7f2270efb1d2ee
IcodeAlpha <alphacode4@outlook.com>
2024-05-29 01:26:34 +0300

    changes

---
+++ components/shared/DeleteConfirmation.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { useTransition } from 'react'
+import { usePathname } from 'next/navigation'
+import Image from 'next/image'
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+
+import { deleteEvent } from '@/lib/actions/event.actions'
+import { Button } from '../ui/button'
+
+export const DeleteConfirmation = ({ eventId }: { eventId: string }) => {
+  const pathname = usePathname()
+  let [isPending, startTransition] = useTransition()
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Image src="/assets/icons/delete.svg" alt="edit" width={20} height={20} />
+      </DialogTrigger>
+
+      <DialogContent className="bg-white">
+        <DialogHeader>
+          <DialogTitle>Are you sure you want to delete?</DialogTitle>
+          <DialogDescription className="p-regular-16 text-grey-600">
+            This will permanently delete this event
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <DialogClose>Cancel</DialogClose>
+
+          <Button
+            onClick={() =>
+              startTransition(async () => {
+                await deleteEvent({ eventId, path: pathname })
+              })
+            }>
+            {isPending ? 'Deleting...' : 'Delete'}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
---
+++ components/shared/Dropdown.tsx
@@ -0,0 +1,90 @@
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+import { ICategory } from "@/lib/database/models/category.model"
+import { startTransition, useEffect, useState } from "react"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+import { createCategory, getAllCategories } from "@/lib/actions/category.action"
+  
+  
+
+type DropdownProps = {
+    value?: string
+    onChangeHandler?: () => void
+}
+
+const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
+    const [categories, setCategories] = useState<ICategory[]>([])
+    const [newCategory, setNewCategory] = useState('');
+
+    const handleAddCategory = () => {
+      createCategory({
+        categoryName: newCategory.trim()
+      })
+        .then((category) => {
+          setCategories((prevState) => [...prevState, category])
+        })  
+    }
+
+    useEffect(() => {
+      const getCategories = async () => {
+        const categoryList = await getAllCategories();
+
+        categoryList && setCategories(categoryList as ICategory[])
+      }
+
+      getCategories();
+    }, [])
+
+
+  return (
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+        <SelectTrigger className="select-field">
+            <SelectValue placeholder="Category" />
+        </SelectTrigger>
+        <SelectContent>
+            {categories.length > 0 && categories.map((category) => (
+                <SelectItem key={category._id} value={category._id}
+                className="select-item p-regular-14">
+                    {category.name}
+                </SelectItem>
+            ))}
+
+<Dialog>
+  <DialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">Add new category</DialogTrigger>
+  <DialogContent className="bg-white">
+    <DialogHeader>
+      <DialogTitle>New Category</DialogTitle>
+      <DialogDescription>
+        <Input type="text" placeholder="Category name" className="input-field mt-3" onChange={(e) => setNewCategory(e.target.value)}/>
+      </DialogDescription>
+    </DialogHeader>
+    <DialogFooter>
+      <DialogClose>Cancel</DialogClose>
+      <Button onClick={() => startTransition(handleAddCategory)}>Add</Button>
+    </DialogFooter>
+  </DialogContent>
+</Dialog>
+   
+        </SelectContent>
+    </Select>
+
+  )
+}
+
+export default Dropdown
